d1a55a9161fd7d2d10caf1ca70e980bfd2ac08e2
meron <malemu17@yahoo.com>
2019-04-23 14:43:55 -0700

    pagination

--- js/app.js
+++ js/app.js
@@ -1,218 +1,180 @@
 'use strict';
 
-function Gallery(horn){
-    for (let key in horn){
-        this[key]=horn[key];
+// for each item in array we will search for keywords. keywords will be linked to keywords in our drop down men
+// creating a funciton that will creat unique object for our gallery, based on the data it will read from the local json file
+
+function Gallery(horn) {
+    // linking each attribute to the corresponding value in the json file
+    for (let key in horn) {
+
+        this[key] = horn[key];
     }
 }
+// once we create the object, we will store them in this array to call through as needed, but separately for each json page
+Gallery.allHorns1 = [];
+Gallery.allHorns2 = [];// nav handler
 
-Gallery.allHorns = [];
-Gallery.keyword = [];
+$('nav a').on('click', function () {
+    let $oneOrTwo = $(this).data('tab');
+    // what is $whereToGo
+    // gives us 'delegation' or 'attributes'
+    console.log('page one or page two', $oneOrTwo);
+    $('.content').hide();
+    // we want $('#delegation')
+    $('#' + $oneOrTwo).fadeIn(750)
 
+})
 
-Gallery.prototype.toHtml = function(){
-    let $template =Handlebars.compile($template);
-    let compiledTemplate = Handlebars.compile($template);
+Gallery.prototype.toHtml = function () {
+    // we are going to get main from the dom, and inside of main we are going to add in new div elements conatining data from out gallery objects--now with handlebars...
+    let $hbarTemplate = $('#horns-template').html();
+    // console.log('template source', $hbarTemplate);
+    // the copied html pattern is now the skeleton of our newly created horn item div
+    let compiledTemplate = Handlebars.compile($hbarTemplate);
+    // console.log(compiledTemplate);
     return compiledTemplate(this);
-};
+}
 
+// one page at a time
 
-Gallery.readJson = ($value)=> {
-    $.get(`data/${$value}.json`, 'json')
-    .then(data=>{
-        data.forEach(horn =>{
-            Gallery.allHorns.push(new Gallery(horn));
-        });
+let jp1 = '/lab-02-repository/data/page-1.json';
+
+let jp2 = '/lab-02-repository/data/page-2.json';
+
+let tab1 = $('#one');
+
+let tab2 = $('#two');
+// now we need to get the data to run this 
+
+Gallery.readJson = (jpage, galleryset, place, filter) => {
+    // we are going to grab a json file to run through our constructor
+    $.get(jpage, 'json')
+        // s ogo and get that info then...
+        .then(data => {
+            // for each item we have in json we are going to run through our constructor and push it into our gallery catalogue
+            data.forEach(item => {
+                galleryset.push(new Gallery(item));
+            })
+        })
+        //then we want ot send them to the html
+        .then(place)
+        .then(Gallery.loadKeywords)
+        // .then(Gallery.populateFilter)
+        .then(filter)
+}
+
+Gallery.loadHorns1 = () => {
+    Gallery.allHorns1.forEach(hornItem => {
+        $('#one').append(hornItem.toHtml());
     })
-
-    .then(populateKeywords)
-    .then(sortKeywords)
-    .then(Gallery.loadHorns)
-    .then(Gallery.loadKeywords)
 }
-
-
-function populateKeywords( ){
-    Gallery.allHorns.forEach(horn=> {
-        if(!Gallery.keyword.includes(horn.keyword)){
-            Gallery.keyword.push(horn.keyword)
-        }
+Gallery.loadHorns2 = () => {
+    Gallery.allHorns2.forEach(hornItem => {
+        $('#two').append(hornItem.toHtml());
     })
 }
+Gallery.loadKeywords = () => {
 
-function sortKeyword() {
-    Gallery.keyword.sort()
+    let filterKeywords = [];
 
-}
+    $('option').not(':first').remove();
 
+    Gallery.allHorns1.forEach(horn => {
 
-Gallery.loadHorns = () =>{
-    Gallery.allHorns.forEach(horn => {
-        $('#photo-template').append(horn.toHtml())
+        if (!filterKeywords.includes(horn.keyword))
+
+            filterKeywords.push(horn.keyword);
+
     });
-};
+
+    Gallery.allHorns2.forEach(horn => {
+
+        if (!filterKeywords.includes(horn.keyword))
+
+            filterKeywords.push(horn.keyword);
+
+    });
+
+    let filterkeywords = [];
+
+    filterkeywords.sort();
 
 
-$(() => Gallery.readJson($value));
-  let $value = 'page-1';
 
-  Gallery.loadKeyword = () => {
+    filterKeywords.forEach(keyword => {
 
-    Gallery.keyword.forEach((keyword)=> {
-        $('#filter-key').append(`<option class="filter-remove" value="${keyword}">${keyword}</option`);
+        let optionTag = `<option value = "${keyword}">${keyword}</option>`;
+
+        $('select').append(optionTag);
+
+        console.log(filterKeywords);
+
+    });
+
+}
+Gallery.handleFilter1 = () => {
+
+    $('select').on('change', function () {
+
+        let $selected = $(this).val();
+
+        console.log('selected is ', $selected);
+
+        if ($selected !== 'default') {
+
+            Gallery.allHorns1.forEach(horn => {
+                if ($selected === horn.keyword) {
+                    console.log($selected);
+                    $('div').attr("style", "display: none");
+                    $(`.${$selected}`).attr("style", "display: block")
+
+                    console.log('here i am at ', horn.keyword, $selected);
+
+                }
+
+            });
+
+        }
+
     })
 
-  };
- 
-$('#filter').on('change', function(){
-    let $selection = $(this).val();
-    $('div').hide();
-    $('div[class="${$selection}"]').show();
-});
+}
+Gallery.handleFilter2 = () => {
 
+    $('select').on('change', function () {
 
+        let $selected = $(this).val();
 
-$('#click').on('change', function() {
+        console.log('selected is ', $selected);
 
-  $('.filter-remove').remove();
+        if ($selected !== 'default') {
+            Gallery.allHorns2.forEach(horn => {
+                if ($selected === horn.keyword) {
 
-  $('div').remove();
+                    console.log($selected);
 
-  let $value = $(this).val();
+                    $('div').attr("style", "display: none");
 
-  Gallery.allHorns = [];
+                    $(`.${$selected}`).attr("style", "display: block")
 
-  Gallery.keywords = [];
+                    console.log('here i am at ', horn.keyword, $selected);
 
-  Gallery.readJson($value);
+                }
 
-});
+            });
 
+        }
 
+    })
 
+}
 
-// // for each item in array we will search for keywords. keywords will be linked to keywords in our drop down menu
+$(() => Gallery.readJson(jp1, Gallery.allHorns1, Gallery.loadHorns1, Gallery.handleFilter1));
+$(() => Gallery.readJson(jp2, Gallery.allHorns2, Gallery.loadHorns2, Gallery.handleFilter2));
+// DOM-ready function
 
+$(document).ready(function () {
 
-// // creating a funciton that will creat unique object for our gallery, based on the data it will read from the local json file
-// function Gallery(horn) {
-//     // linking each attribute to the corresponding value in the json file
-//     this.title = horn.title;
-//     this.keyword = horn.keyword;
-//     this.horns = horn.horns;
-//     this.image_url = horn.image_url;
-//     this.description = horn.description;
-// }
+    $('#two').hide()
 
-// // once we create the object, we will store them in this array to call through as needed
-// Gallery.allhorns=[];
-
-
-
-// Gallery.prototype.render = function () {
-//     $('main').append('<div class="clone"></div>');
-//     let hornClone = $('div[class="clone"]');
-
-
-//     // we are going to get main from the dom, and inside of main we are going to add in new div elements conatining data from out gallery objects
-
-//     let hornItemHtml = $('#photo-template').html();
-// // the copied html pattern is now the skeleton of our newly created horn item div
-//     hornClone.html(hornItemHtml);
-
-//     // at this point we will find each element and rewrite it
-//     hornClone.find('h2').text(this.title);
-//     hornClone.find('img').attr('src', this.image_url);
-//     hornClone.find('p').text(this.description);
-//     hornClone.attr('class',this.keyword);
-//     // hornClone.attr('class','img');
-//     hornClone.removeClass('clone');
-// }
-
-// // now we need to get the data to run this operation
-// Gallery.readJson = () =>{
-//     // we get json file form our dir
-//     $.get('./page-1.json', 'json')
-
-
-//         .then(data => {
-//             data.forEach(item => {
-//                 Gallery.allHorns.push(new Gallery(item));
-//             })
-//         })
-
-//         .then(Gallery.loadHorns)
-//         .then(Gallery.loadKeywords)
-//         .then(Gallery.populateFilter)
-//         .then(Gallery.handleFilter)
-
-// }
-
-
-// Gallery.loadHorns = () => {
-//     Gallery.allHorns.forEach(horn => horn.render())
-// }
-
-
-// Gallery.loadKeywords = () => {
-//     let filterKeywords = [];
-//     $('option').not(':first').remove();
-//     Gallery.allHorns.forEach(horn => {
-//         if (!filterKeywords.includes(horn.keyword))
-//         filterKeywords.push(horn.keyword);
-//     });
-
-//     let filterkeywords = [];
-//     filterkeywords.sort();
-
-//     filterKeywords.forEach(keyword => {
-//         let optionTag = `<option value = "${keyword}">${keyword}</option>`;
-//         $('select').append(optionTag);
-//     });
-// }
-
-// Gallery.handleFilter = () => {
-//     $('select').on('change', function(){
-//         let $selected=$(this).val();
-//         console.log('selected is ',$selected);
-//         if($selected!== 'default'){
-
-         
-            
-//             Gallery.allHorns.forEach(horn=>{
-                
-//                 if($selected===horn.keyword){
-//                     console.log($selected);
-//                     $('div').attr("style", "display: none");
-//                     $(`.${$selected}`).attr("style", "display: block");
-//                 }
-//             });
-//             //$(`option[value=${$selected}]`).fadeIn();
-//         // }else{
-//         //     $('div').removeClass('filtered').fadeIn();
-//         //     $(`option[value=${$selected}]`).fadeIn();
-//         }
-//     })
-// }
-
-// $(() => Gallery.readJson());
-
-// // // Configure an object to hold all of our functions for dynamic updates & event handlers 
-// // let gallareyView = {};
-
-// // galleryView.populateFilters = function() {
-// //     $('gallery').each(function() {
-// // let title, description, keyword, optionTag;
-
-// // if (!$(this).hasClass('photo-template')) {
-
-// //     title = $(this).attr('data-gallery');
-
-//    // DONE: Refactor this concatenation using a template literal.
-
-//    optionTag = `<option value= "${keyword}"> ${keyword} </option>`;
-//    if ($('#filter-key option[value="' + filter-key + '"]').length === 0) {
-       
-//     $('#filter-key').append(optionTag);
-
-//    }
+})
